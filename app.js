ebb40e7a8353d7935704a733b1f48001d00f333c
Arman Bijoy <armanbijoy4@gmail.com>
2022-01-20 08:37:31 +0600

    Added

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ const yargs = require('yargs')
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function () {
-    console.log('Adding A New Note...')
+  builder: {
+    title: {
+      describe: 'Note Title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note Body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
   },
 })
 
@@ -33,4 +46,5 @@ yargs.command({
     console.log('Reading a note')
   },
 })
-console.log(yargs.argv)
+// console.log(yargs.argv)
+yargs.parse()
